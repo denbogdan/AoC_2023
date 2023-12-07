c5518b7d497ff671b8164bca16a974c2585dbaa4
Denisa Bogdan <dbogdan@DMBP14.local>
2023-12-07 01:30:43 +0000

    day 3 solution - not working on input

--- day_5/day_5.R
+++ day_5/day_5.R
@@ -32,57 +32,26 @@ seeds <- seeds[2:length(seeds)] |> as.numeric()
 
 #write function that takes a number and a map and produces its new equivalent
 value_to_map <- function(n, map) {
-  #find line corresponding to the right interval
-  for(i in 1:nrow(map)) {
-    row <- map[i,]
-    start <- row["source"]
-    end <- row["source"] + row["range_len"]
-    if((n >= start) & (n<=end)) break
-  }
-  #find the increment 
-  inc <- n-start
-  #calculate mapped value
-  mapped <- row["destination"] + inc
-  return(mapped)
-}
-
-start_time <- Sys.time()
-#map all seeds to the end 
-locations <- c()
-for(i in 1:length(seeds)) {
-  n <- seeds[i]
-  #go through all the maps 
-  for(j in 1:length(maps_list)) 
-    n <- value_to_map(n, maps_list[[j]])
-  #append
-  locations <- c(locations, n)
-}
-end_time <- Sys.time()
-
-end_time - start_time
-
-##Answer 1 -------------------------------------------------------------------------
-locations |> as.numeric() |> min()
-
-
-##Question 2 -----------------------------------------------------------------------
-#how many new seed numbers?
-sum(seeds[seq(2, length(seeds), 2)])
-
-value_to_map <- function(n, map) {
   #find start of source
   map <- map[order(map$source),]
   map$source_end <- map$source + map$range_len
   source_end<- map$source_end[which(map$source_end>=n)]
-  source_end <- source_end[1]
-  source_start <- map[which(map$source_end == source_end), "source"]
-  #find the increment 
-  inc <- n-source_start
-  #calculate mapped value
-  mapped <- map[which(map$source == source_start), "destination"] + inc
+  if(length(source_end) == 0)
+    mapped <- n
+  else {
+    source_end <- source_end[1]
+    source_start <- map[which(map$source_end == source_end), "source"]
+    if(n < source_start) {
+      mapped <- n
+    } else {
+      inc <- n-source_start
+      mapped <- map[which(map$source == source_start), "destination"] + inc
+    }
+  }
   return(mapped)
 }
 
+#solve for all seeds
 start_time <- Sys.time()
 #map all seeds to the end 
 locations <- c()
@@ -100,4 +69,13 @@ end_time <- Sys.time()
 
 end_time - start_time
 
+##Answer 1 -------------------------------------------------------------------------
 locations |> as.numeric() |> min()
+
+
+##Question 2 -----------------------------------------------------------------------
+#check number of new seeds
+sum(seeds[seq(2, length(seeds), 2)])
+
+#work backwards - find lowest locations in the last map
+
